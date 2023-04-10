4b86442dab4931ca86582c84d01a6976d632eaba
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-04-10 00:28:59 -0500

    adding no-cursor effect

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,5 +1,78 @@
-function App () {
-  return <h3>Project 3</h3>
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log({ clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // getEventListeners(window)
+
+    // - when unmount component
+    // - when dependency changes, before to execute the effect again
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  const handleClick = () => {
+    setEnabled(!enabled)
+  }
+
+  useEffect(() => {
+    /* if (enabled) {
+      document.body.classList.add('no-cursor')
+    } */
+
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+      <button onClick={handleClick}>
+        {enabled ? 'Deactivate' : 'Activate'} follow pointer
+      </button>
+    </main>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
 }
 
 export default App
