91bb05155cb3ab8e2b9eabb25193ca1c2deb6fcb
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2023-04-09 23:22:13 -0500

    adding comment

--- projects/02-tic-tac-toe/src/App.tsx
+++ projects/02-tic-tac-toe/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import confetti from 'canvas-confetti'
 import { Square } from './components/Square'
 import { TURNS } from './constants'
@@ -9,6 +9,12 @@ function App() {
   /* const board =  */
   const [board, setBoard] = useState<string[]>(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+
+  // const [turn, setTurn] = useState(() => {
+  //   const turnFromStorage = window.localStorage.getItem('turn')
+  //   return turnFromStorage ?? TURNS.X
+  // })
+
   const [winner, setWinner] = useState<string | boolean | null>(null) // false means tie
 
   const updateBoard = (index: number) => {
@@ -22,6 +28,10 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
+    // save game
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+
     // check winner
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
@@ -32,10 +42,24 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    const board = window.localStorage.getItem('board')
+    if (board) {
+      setBoard(JSON.parse(board))
+    }
+    const turn = window.localStorage.getItem('turn')
+    if (turn) {
+      setTurn(turn)
+    }
+  }, [])
+
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   return (
